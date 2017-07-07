583828b674631cc4ba579258be75cb3bfbac6280
Ravend20 <Fleetsd@yahoo.com>
2017-07-07 15:48:30 -0400

    Worked on UserInput Aka Rudeboy and HelloDemo

---
+++ HelloDemo/HelloDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace HelloDemo
+{
+    class Program
+    {
+        /// <summary>
+        /// I need to figure out else statements to have conversations with myself.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Greetings");
+            //Console.ReadKey();
+            //I want an else statement here
+           Console.WriteLine("We are The Borg");
+            // And else statement here
+            Console.WriteLine("Resistance is futile");
+            //Console.WriteLine("We will add your technological likenesses to our own.");
+            //Console.WriteLine("Prepare to be assimilated");
+            Console.WriteLine("ALL YOUR BASS BELONGS TO US");
+
+            Console.ReadKey();
+            WriteLine("We are coming");
+            Write("Today\n");
+            Write("..NOW");
+            ReadKey();
+
+    
+
+
+           
+        }
+
+    }
+}
---
+++ UserInput/UserInput/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInput
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            // Rudeboy
+
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name + " it is a good day to die");
+            Console.ReadLine();
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            Console.WriteLine("Wow youre old...");
+            Console.Write("Im sorry that was mean... \n");
+            Console.WriteLine(age + " Years old person named " + name + " ..Hmm..Well atleast you look your age.");
+            Console.Write("Hahaha... You wont be missed\n");
+            Console.Write("Are you going out tonight: ?");
+            string goingout = Console.ReadLine();
+        
+            //sting interpolation
+            Console.WriteLine($"Your name is {name} and youre {age} of course youre {goingout}");
+        
+            Console.ReadKey();
+
+
+
+
+
+
+          
+
+        }
+    }
+}
