5a3504c57e2fade17cdd7eac6595b5710334cdaa
Ravend20 <Fleetsd@yahoo.com>
2017-07-12 15:32:44 -0400

    switch work on stuff

--- SwitchPractice/SwitchPractice/Program.cs
+++ SwitchPractice/SwitchPractice/Program.cs
@@ -10,6 +10,23 @@ namespace SwitchPractice
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("what is you favorite color");
+            string color = Console.ReadLine();
+
+
+            switch(color)
+            {
+
+                case "red":
+                    Console.WriteLine("Red is an ok color");
+                    break;
+
+                default:
+                    Console.WriteLine("we dont understand yur answer");
+                    break; 
+
+            }      
+
         }
     }
 }
