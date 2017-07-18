7c8cc78fada3b2a0882f8b93bcec559d23734a57
Ravend20 <Fleetsd@yahoo.com>
2017-07-18 09:44:10 -0400

    first example of while loops

---
+++ WhileLoopPractice1/WhileLoopPractice1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoopPractice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string userName = "Locutus of Borg";
+            string password = "assimilation";
+            Console.WriteLine("What is your username?");
+           string inputName = Console.ReadLine();
+            Console.WriteLine("What is your password");
+            string pw = Console.ReadLine();
+
+            while (inputName != userName || pw != password)
+            {
+                Console.WriteLine($"Your response is not the correct one {inputName}. Get off the computer poser...Uploading location now.");
+                Console.WriteLine("Retry?");
+                inputName = Console.ReadLine(); 
+                pw = Console.ReadLine();
+            }
+            Console.WriteLine($"Correct username and password were correct you may proceed {inputName}.");
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
