ac89a70bc2f591fe0eabec62f86b591b979fd6dd
Ravend20 <Fleetsd@yahoo.com>
2017-07-18 12:38:58 -0400

    Still hate FizzBuzz

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -10,42 +10,25 @@ namespace FizzBuzz
     {
         public static void Main(string[] args)
         {
-
-
-          // $step[0] = 3;
-         //  $step[1] = 5;
-         //   $step[2] = 15;
-
-
             for (int i = 1; 1<= 100; i++)
             {
-
                 if (i % 3 == 0)
                 {
                    Console.WriteLine("fizz");
-                }
-                             
+                }                             
                 else if (i % 5 == 0)
                 {
                   Console.WriteLine("buzz");
-                }
-                  
+                }                 
                 else if (i % 15 == 0)
                 {
                   Console.WriteLine("fizzbuzz");
-                }
-                 
+                }                 
                 else
                 {
                   Console.WriteLine(i);
-                }
-                  Console.ReadKey();
-              
-
-
-
-
-
+                }               
+                Console.ReadKey();             
             }
 
 
