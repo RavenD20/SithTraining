072ce5548b860e6e086f92c1f4dfefa1c46f7920
Ravend20 <Fleetsd@yahoo.com>
2017-07-18 16:26:23 -0400

    test calculator.. eh

---
+++ GuessingGame1/GuessingGame1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int rNum = rand.Next(1, 20);
+            int realNum;
+            Console.WriteLine("Enter a number between 1 and 20");
+            string numGuess = Console.ReadLine();
+            int.TryParse(numGuess, out realNum);
+
+
+            while(realNum != rNum)
+            {
+                Console.WriteLine($"Your guess of {numGuess} was not the correct number...\n guess again");
+
+                numGuess = Console.ReadLine();
+
+                int.TryParse(numGuess, out realNum);
+            }
+                              
+                    Console.WriteLine($"Youre guess of {rNum} was correct...Hooray");
+
+
+                    numGuess = Console.ReadLine();
+                
+               
+
+
+        }
+    }
+}
---
+++ TScores/TScores/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TScores
+{
+    class Program
+    {
+        static void Main(string[] args)
+           
+            
+        {
+         
+
+            int testnum;
+            int lowNum = 0;
+            int[] bonus = { 20 };
+            int highNum = 100;
+
+
+
+         
+           
+            
+           
+            Console.WriteLine("What is your test score?");
+            string valNum = Console.ReadLine();
+            int.TryParse(valNum, out testnum);
+
+            while (testnum < lowNum || testnum > highNum)
+            {
+                Console.WriteLine("The test score you entered isnt valid. What is your score?");
+                valNum = Console.ReadLine();
+                int.TryParse(valNum, out testnum);
+            }
+            Console.WriteLine($"Your test score is {testnum}");
+              for(int z =0; z < bonus.Length; ++z)
+            {
+              //Console.WriteLine($"You grand total is {testnum}");
+                testnum += bonus[z];
+            }
+            Console.WriteLine($"The total score is {testnum}");
+            Console.ReadLine();
+
+
+
+
+
+
+
+        }
+    }
+}
