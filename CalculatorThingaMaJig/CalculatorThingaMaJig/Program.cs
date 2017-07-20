036cdb4d9e444ec382d80b00b1e9220900f872f8
Ravend20 <Fleetsd@yahoo.com>
2017-07-20 16:34:17 -0400

    Git it done lol

--- CalculatorThingaMaJig/CalculatorThingaMaJig/Program.cs
+++ CalculatorThingaMaJig/CalculatorThingaMaJig/Program.cs
@@ -10,6 +10,46 @@ namespace CalculatorThingaMaJig
     {
         static void Main(string[] args)
         {
+
+            int val1;
+            int val2;
+            int Mathstuff;
+
+
+            Console.WriteLine("Welcome to the Calculatron.");
+            Console.Write("Your name please?");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Hello! How may I assist you today {name}?");
+            Console.Write("Would you would like to: 1. Add \n 2. Subtract \n 3. Multiply \n 4. Divide");
+            string assist = Console.ReadLine();
+
+            int.TryParse(assist, out Mathstuff);
+
+
+
+            switch (Mathstuff)           
+            {
+                case 1:
+                   Console.WriteLine("");
+                    break;
+
+
+
+                
+
+
+
+
+
+
+
+
+            }
+
+
+
         }
     }
 }
