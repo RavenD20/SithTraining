5df3784cc3c8379d0c1db48aaecf1317b8dee10a
Ravend20 <Fleetsd@yahoo.com>
2017-07-20 11:59:47 -0400

    Why are these methods trying to be so annoying. Need to revisit.

---
+++ MethodPrac1/MethodPrac1/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPrac1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            restaurants r = new restaurants();
+
+            Fashion Fash = new Fashion();
+            Fashion F = new Fashion();
+
+
+
+            Console.WriteLine("This is the main method landing page");
+
+            r.Chipotle();
+            F.pants();
+            F.shoes();
+            r.Benihana();
+           
+        }
+
+        
+
+        public static void Addition()
+        {
+
+
+
+
+        }
+
+        public static void Multiplication()
+        {
+
+
+
+
+
+
+        }
+
+        public static void division()
+        {
+
+
+
+
+        }
+     class Fashion
+    {
+
+            public static void shirts()
+            {
+
+                Console.WriteLine("Button down");
+
+
+
+
+
+
+            }
+
+            public void pants()
+            {
+
+
+                Console.WriteLine("No pants no service");
+
+
+            }
+
+
+            public void shoes()
+            {
+                Console.WriteLine(" Shoes?..Yes please we dont like cornchips.");
+
+                Console.WriteLine("Except on tacos");
+
+            }
+    }
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ MethodPrac1/MethodPrac1/restaurants.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPrac1
+{
+    class restaurants
+    {
+       public static void FamousDaves()
+        {
+
+            
+            
+           restaurants r = new restaurants();
+
+            Program p = new Program();
+
+            
+           
+
+
+
+        }
+
+        public void Chipotle()
+        {
+
+            Console.WriteLine("We make burritos");
+
+        }
+        public void Benihana()
+        {
+
+
+            Console.WriteLine(" We cook the food right in front of you");
+
+            
+        }
+
+
+
+
+    }
+}
