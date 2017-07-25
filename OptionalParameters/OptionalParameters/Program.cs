a46e7472c02065cbd58c15997a3a975f0a374c3d
Ravend20 <Fleetsd@yahoo.com>
2017-07-25 12:30:22 -0400

    optional Parameters

---
+++ OptionalParameters/OptionalParameters/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Using 2 arguments here!!!");
+            DisplaySize(10, 20);
+            Console.WriteLine("Using 3 arguements now");
+            DisplaySize(1, 2, 3);
+            Console.ReadLine();
+        }
+
+        public static void DisplaySize(int length, int width, int height = 1)
+        {
+            int area = length * width * height;
+            Console.WriteLine($"the {length} * the {width} * the optional height of {height}  equals {area}");
+            Console.ReadLine();
+
+
+        }
+    }
+}
