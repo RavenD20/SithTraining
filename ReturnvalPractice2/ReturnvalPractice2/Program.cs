92078fecea28219145f7d7727e7b69660693346e
Ravend20 <Fleetsd@yahoo.com>
2017-07-27 16:37:49 -0400

    need to finish the birthday an math

--- ReturnvalPractice2/ReturnvalPractice2/Program.cs
+++ ReturnvalPractice2/ReturnvalPractice2/Program.cs
@@ -10,6 +10,52 @@ namespace ReturnvalPractice2
     {
         static void Main(string[] args)
         {
+
+            
+            
+            string month = "";
+         //   string age = "";
+           string year = "";
+            string favplace = "";
+            string favfood = "";
+
+
+            string myfavorites = Preference(favplace, favfood);
+
+            Console.WriteLine($"The place you like to go is {favplace} and your favorite food is {favfood}");
+            
+            Console.ReadLine();
+
+
+            //Console.WriteLine("What year is it?");
+            //year = Console.ReadLine();
+
+            //Console.WriteLine("How old are you?");
+            //string age = Console.ReadLine();
+
+            //Console.WriteLine("What month were you born in?");
+
+
+            //int reply = Birthday(Year,age);
+            //Console.WriteLine("Your birthday is..");
+            //Console.Write(reply);
+            //Console.ReadLine();
         }
-    }
+
+
+        public static int Birthday(int n, int n2)
+        {
+
+            //int DateofBirth = year - age;
+            //return DateofBirth;
+
+        }
+        private static string Preference( string place, string food)
+        {
+            string WhatILike = place + food;
+            return WhatILike;
+           
+        }
+
+    }       
 }
