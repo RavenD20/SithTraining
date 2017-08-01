75ebe58e1cc4d511ffd77e18ce4dc4bb2583cc07
Ravend20 <Fleetsd@yahoo.com>
2017-08-01 16:06:29 -0400

    saving changes before it crashes again

---
+++ EncapsulationPractice1/EncapsulationPractice1/EncapsulationId.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationPractice1
+{
+    class Student
+    {
+
+        private int idNumber;
+        private string lastName;
+        private string firstName;
+
+
+        public int IdNumber
+        {
+
+            get
+            {
+
+                return idNumber;
+
+            }
+            set
+            {
+
+                IdNumber = value;
+
+            }
+
+
+
+
+        }
+
+        public string LastName
+        {
+
+            get
+            {
+
+                return LastName;
+
+
+            }
+            set
+            {
+
+                LastName = value;
+
+            }
+
+
+
+
+
+
+        }
+
+        public string FirstName
+        {
+
+            get
+            {
+                return FirstName;
+
+
+            }
+
+            set
+            {
+
+                FirstName = value;
+
+            }
+
+
+
+
+        }
+
+
+    }
+}
---
+++ EncapsulationPractice1/EncapsulationPractice1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationPractice1
+{
+    class program
+    {
+       
+       static void Main(string[] args)
+        {
+            Student s = new Student();
+            Student s1 = new Student();
+            Student s2 = new Student();
+
+            s.IdNumber = 1001;
+            s.FirstName = "Diana";
+            s.LastName = "Prince";
+
+        }
+
+       public void ShowStudent(Student scholar)
+        {
+
+            Console.WriteLine($"My Id Number is {scholar.IdNumber}, my first name is {scholar.FirstName} and my lastname is {scholar.LastName}");
+
+
+        }
+                
+                
+                
+       
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+        
+                            
+            
+                       
+       
+    }
+}
