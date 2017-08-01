ac0212ff11af2819c682db2746413737488914dc
Ravend20 <Fleetsd@yahoo.com>
2017-08-01 11:33:13 -0400

    Abstract Practice that works

---
+++ AbstractPractice1/AbstractPractice1/car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractPractice1
+{
+    class Car
+    {
+
+        public int mileage = 60;
+
+
+        public String color = "Red";
+
+
+        private string formula = "a+b";
+
+        public int GetMileage()
+        {
+            return mileage;
+
+
+
+        }
+        public string GetColor()
+        {
+            return color;
+
+        }
+
+        private string GetEngineFormula()
+        {
+
+            return formula;
+
+        }
+
+        public string DisplayEngineFormula()
+        {
+            return GetEngineFormula();
+
+
+        }
+
+    }
+}
