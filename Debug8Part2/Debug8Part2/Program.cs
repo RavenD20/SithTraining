20f0906c7f4e28f123b5724bbabdd2a4060ed58f
Ravend20 <Fleetsd@yahoo.com>
2017-08-07 11:48:14 -0400

    ....Look over later

---
+++ Debug8Part2/Debug8Part2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debug8Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+
+           // DebugEight3.EightThree();
+            DebugEight4.EightFour();
+        }
+    }
+}
