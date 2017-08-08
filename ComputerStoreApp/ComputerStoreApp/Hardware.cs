3bbac37098c380a3e4f531917838496f005b6b29
Ravend20 <Fleetsd@yahoo.com>
2017-08-08 12:30:44 -0400

    Finish Later.

---
+++ ComputerStoreApp/ComputerStoreApp/Hardware.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerStoreApp
+{
+    class Hardware
+    {
+        public int PartID { get; set; }
+        public string PartType { get; set; }
+        public string PartName  { get; set; }
+        public bool Internal { get; set; }
+        public bool External { get; set; }
+        public double PreformanceRating { get; set; }
+
+
+        public void CompGear()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+
+
+        }
+
+
+    }
+}
