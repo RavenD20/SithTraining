a0b22afeaf0aaa48e1639decff6073b2519511f6
Ravend20 <Fleetsd@yahoo.com>
2017-08-09 01:53:48 -0400

    if statement issues

---
+++ DebugNine4/DebugNine4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebugNine4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            FixedDebugNine4.DebugNineFour();
+
+
+        }
+    }
+}
