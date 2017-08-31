73e09457628b2739f460ebf64e5ddaad2528369c
Ravend20 <Fleetsd@yahoo.com>
2017-08-31 00:16:05 -0400

    I cant believe I made that harder than it was

---
+++ querypractice/Scripts/JsScript.js
@@ -0,0 +1,75 @@
+﻿
+
+//var person = {
+//    "name": "Brad"
+//}
+//console.log(person.name);
+
+
+
+
+
+
+
+
+
+
+
+
+
+function pow() {
+
+    var gord = { city: "Cleveland", team: "Cavaliers", arena: "Quicken Loans", star: "Lebron", owner: "Gilbert", co: "Usher", };
+    document.getElementById('show').innerHTML = " Our hometown is " + gord.city + " & our superstar is " + gord.star;
+
+
+
+    //htmlString;
+
+}
+    //for (i = 0; ii < gord[i].city.star; ii++) {
+    //    htmlString += "show" + "Our hometown is" + gord[i].city + "& our superstar is" + gord[i].star
+    //}
+    //    //go = gord;
+
+    //}
+    //go = gord;
+    //    var ourRequest = new XMLDocument();
+    //ourRequest.open('GET', 'querypractice\AjaxPractice.txt')
+    //ourRequest.onload = function kaboom() {
+    //    var cars = JSON.parse(ourRequest.responseText);
+    //    console.log(gord[data])
+    //};
+    //ourRequest.send();
+
+    //function renderHtml(data){
+    //    var htmlString ="";
+    //}
+
+
+
+    function bang() {
+
+        var zorro = { Infiniti: "FX 35", ford: "Ranger", Chevy: "Corvette", Nissan: "Altima" };
+        document.getElementById('display').innerHTML = zorro.Chevy;
+
+    }
+
+
+    function kaboom() {
+
+        var cars = { Nissan: "Altima", ford: "Ranger", Infiniti: "FX 35" };
+        document.getElementById('popup').innerHTML = cars.Nissan ;
+        document.getElementById('done').innerHTML =  cars.ford;
+    }
+
+
+
+
+
+
+
+
+
+
+
