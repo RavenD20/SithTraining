66161969a4ed007be8db98f7aaa91e7e71c3b28d
Ravend20 <Fleetsd@yahoo.com>
2017-09-19 16:03:07 -0400

    jeez

---
+++ MVCTest/MVCTest/Controllers/StartController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTest.Controllers
+{
+    public class StartController : Controller
+    {
+        // GET: Start
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Coffee()
+        {
+            return View();
+        }
+
+        public ActionResult Auto()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ MvcMovie/MvcMovie/Controllers/HelloworldController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+
+namespace MvcMovie.Controllers
+{
+    public class HelloworldController : Controller
+    {
+        // GET: Helloworld
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Arcade()
+        {
+          return View();
+        }
+
+        public ActionResult SpaceInvaders()
+        {
+            return View();
+        }
+
+        
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+        }
+    }
+}
